2f39bff1c7b446cb5581ab83f5bbfc70c52f82f9
Felipe Stein <159493140+FelipeStein1902@users.noreply.github.com>
2025-11-07 08:50:05 -0300

    Mudanças chave no view
    Foi criado a tela para registro de novas contas, atualizada a tela de login para conter o botão de registro, e iniciada a tela inicial.

---
+++ view/Login.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UVV_FINTECH.view;
+
+namespace UVV_FINTECH
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Senha_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        //Verifica no BD se a conta existe
+        private void BotaoEntrar_Click(object sender, RoutedEventArgs e)
+        { 
+            TelaInicial telaInicial = new TelaInicial();
+            telaInicial.Show();
+            this.Close();
+        }
+
+        //Vai para a tela de Registrar conta
+        private void BotaoRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            Registro registro = new Registro();
+            registro.Show();
+            this.Close();
+        }
+    }
+}
---
+++ view/Registro.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UVV_FINTECH.view
+{
+    /// <summary>
+    /// Lógica interna para Registro.xaml
+    /// </summary>
+    public partial class Registro : Window
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void InsiraNovoNome_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        //Registra Nova Conta no BD
+        private void BotaoNovoRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Close();
+        }
+
+        //Se ja possui conta, entra na tela de login
+        private void BotaoPossuiLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Close();
+        }
+    }
+}
